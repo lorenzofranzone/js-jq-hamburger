a7dc9feddff0d765af87b2810f36116b6d7d9c60
Lorenzo Franzone <lorenzo.franzone@gmail.com>
2020-01-16 15:22:30 +0100

    selettori

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-var hamburgerIcon = $('i.fa-bars'); // Recupero l'icona "hamburger"
-var hamburgerIconClose = $('i.fa-times'); // Recupero l'icona "close"
+// var hamburgerIcon = $('i.fa-bars'); // Recupero l'icona "hamburger"
+// var hamburgerIconClose = $('i.fa-times'); // Recupero l'icona "close"
+//
+// // Cliccando sull'icona "hamburger":
+// hamburgerIcon.click (
+//   function () { // Genero la funzione che:
+//     $('.hamburger-menu').show(); // Mostra il menu responsive
+//   }
+// );
+//
+// // Cliccando sull'icona "close":
+// hamburgerIconClose.click (
+//   function () { // Genero la funzione che:
+//     $('.hamburger-menu').hide(); // Nasconde il menu responsive
+//   }
+// );
+
+// Soluzione 2 con selettori diversi
+
+var hamburgerIcon = $('.header-right a'); // Recupero l'icona "hamburger"
+var hamburgerIconClose = $('.close'); // Recupero l'icona "close"
 
 // Cliccando sull'icona "hamburger":
 hamburgerIcon.click (
